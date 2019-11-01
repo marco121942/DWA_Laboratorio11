8569666177058095da458ea6c557968b941e5d13
marco121942 <marco.sotomayor@tecsup.edu.pe>
2019-11-01 04:31:09 -0500

    first commit

---
+++ Infraestructure/Migrations/201911010800029_v2.cs
@@ -0,0 +1,32 @@
+﻿namespace Infraestructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StundentID = c.Int(nullable: false, identity: true),
+                        Codigo = c.String(nullable: false),
+                        StudentName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        FechaCreacion = c.DateTime(),
+                        FechaModificacion = c.DateTime(),
+                        StudentAddress = c.String(nullable: false),
+                        Activo = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.StundentID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
